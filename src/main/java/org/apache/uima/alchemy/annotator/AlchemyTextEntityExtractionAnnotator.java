ec53abe97ac7e051250274a83f025235acfb434d
tommaso.teofili <tommaso.teofili@38d1c562-be07-11de-b431-8336cfaced44>
2009-10-21 20:55:56 +0000

    alchemy parameters read from descriptor

--- src/main/java/org/apache/uima/alchemy/annotator/AlchemyTextEntityExtractionAnnotator.java
+++ src/main/java/org/apache/uima/alchemy/annotator/AlchemyTextEntityExtractionAnnotator.java
@@ -33,51 +33,39 @@ public class AlchemyTextEntityExtractionAnnotator extends JCasAnnotator_ImplBase
 		} catch (Exception e) {
 			throw new ResourceInitializationException(e);
 		}
+		StringBuffer serviceParamsBuf = new StringBuffer();
+		serviceParamsBuf.append("&apikey=");
+		serviceParamsBuf.append(aContext.getConfigParameterValue("apikey"));
+		// param output mode (default xml)
+		serviceParamsBuf.append("&outputMode=");
+		serviceParamsBuf.append(aContext.getConfigParameterValue("outputMode"));
+		// param disambiguate (1 DEFAULT,0)
+	    serviceParamsBuf.append("&disambiguate=");
+	    serviceParamsBuf.append(aContext.getConfigParameterValue("disambiguate"));
+	    // param linked data
+	    serviceParamsBuf.append("&linkedData=");
+	    serviceParamsBuf.append(aContext.getConfigParameterValue("linkedData"));
+	    // param show source text (0 DEFAULT, 1)
+	    serviceParamsBuf.append("&showSourceText=");
+	    serviceParamsBuf.append(aContext.getConfigParameterValue("showSourceText"));
+	    // param base url
+	    serviceParamsBuf.append("&baseUrl=");
+	    serviceParamsBuf.append(aContext.getConfigParameterValue("baseUrl"));
+	    this.serviceParams = serviceParamsBuf.toString();
 	}
 
-	private void initializeParameters(JCas aJCas) {
+	private void initializeRuntimeParameters(JCas aJCas) {
 		// create service parameter string
 	    StringBuffer serviceParamsBuf = new StringBuffer();
-	    
-	    // param api key
-	    serviceParamsBuf.append("&apikey=");
-	    serviceParamsBuf.append("");
-	    
 	    // param text to analyze
 	    serviceParamsBuf.append("&text=");
 	    serviceParamsBuf.append(aJCas.getDocumentText());
-	    
-	    // param url
-//	    serviceParamsBuf.append("&url=");
-//	    serviceParamsBuf.append(url);
-	    
-	    // param output mode (xml DEFAULT, json, rdf, rel-tag, rel-tag-raw)
-	    serviceParamsBuf.append("&outputMode=");
-	    serviceParamsBuf.append("xml");
-	    
-	    // param disambiguate (1 DEFAULT,0)
-//	    serviceParamsBuf.append("&disambiguate=");
-//	    serviceParamsBuf.append(disambiguate);
-	    
-	    // param linked data
-//	    serviceParamsBuf.append("&linkedData=");
-//	    serviceParamsBuf.append(linkedData);
-	    
-	    // param show source text (0 DEFAULT, 1)
-//	    serviceParamsBuf.append("&showSourceText=");
-//	    serviceParamsBuf.append(showSourceText);
-	    
-	    // param base url
-//	    serviceParamsBuf.append("&baseUrl=");
-//	    serviceParamsBuf.append(baseUrl);
-	    
-	    this.serviceParams = serviceParamsBuf.toString();
+	    this.serviceParams+=(serviceParamsBuf.toString());
 	}
 
 	public void process(JCas aJCas) throws AnalysisEngineProcessException {
 		//initialize service parameters 
-		initializeParameters(aJCas);
-		
+		initializeRuntimeParameters(aJCas);
 	    try {
 	      // open connection and send data
 	      URLConnection connection = this.alchemyService.openConnection();
@@ -100,7 +88,8 @@ public class AlchemyTextEntityExtractionAnnotator extends JCasAnnotator_ImplBase
 	      DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
 	      BufferedInputStream in = new BufferedInputStream(connection.getInputStream());
 	      Document feedDoc = docBuilder.parse(in);
-	      String rdfXmlContent = feedDoc.getDocumentElement().getTextContent();
+	      String xmlContent = feedDoc.getDocumentElement().getTextContent();
+	      System.out.println(xmlContent);
 
 	    }
 	    catch (Exception e) {
