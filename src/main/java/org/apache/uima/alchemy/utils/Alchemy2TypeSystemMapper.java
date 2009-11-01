4911b140f171f2130491b3e3e667a71aa2b8248b
tommaso.teofili <tommaso.teofili@38d1c562-be07-11de-b431-8336cfaced44>
2009-11-01 09:59:34 +0000

    first mapping of alchemy types to uima type system finished

--- src/main/java/org/apache/uima/alchemy/utils/Alchemy2TypeSystemMapper.java
+++ src/main/java/org/apache/uima/alchemy/utils/Alchemy2TypeSystemMapper.java
@@ -2,32 +2,26 @@ package org.apache.uima.alchemy.utils;
 
 import org.apache.uima.alchemy.digester.domain.Entity;
 import org.apache.uima.alchemy.digester.domain.Results;
-import org.apache.uima.cas.CASException;
-import org.apache.uima.cas.CASRuntimeException;
+import org.apache.uima.alchemy.utils.exception.MappingException;
 import org.apache.uima.cas.FeatureStructure;
 import org.apache.uima.jcas.JCas;
 
 public class Alchemy2TypeSystemMapper {
 
-	public static void mapEntities(Results results, JCas aJCas) {
+	public static void mapEntities(Results results, JCas aJCas) throws MappingException{
 		
 		for (Entity entity : results.getEntities().getEntities()) {
-			System.out.println("en:"+entity.getText()+"+"+entity.getType()+"+"+entity.getCount()+"+"+entity.getRelevance());
 			try {
-				//FIXME create type system classes first!
-				Object fsObject = ("org.apache.uima.alchemy.ts.entity."+entity.getType()).getClass();
+				//use reflection to instantiate classes of the proper type in the type system
+				Object fsObject = Class.forName("org.apache.uima.alchemy.ts.entity."+entity.getType()).getConstructors()[1].newInstance(aJCas);
 				FeatureStructure fs = (FeatureStructure) fsObject;
 				fs.setFeatureValueFromString(aJCas.getRequiredFeature(fs.getType(), "count"), entity.getCount()); //count
 				fs.setFeatureValueFromString(aJCas.getRequiredFeature(fs.getType(), "text"), entity.getText()); //text
 				fs.setFeatureValueFromString(aJCas.getRequiredFeature(fs.getType(), "relevance"), entity.getRelevance()); //relevance
 				//TODO handle disambiguation
 				aJCas.addFsToIndexes(fs);
-			} catch (CASRuntimeException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (CASException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			} catch (Exception e) {
+				throw new MappingException(e);
 			}
 		}
 		
