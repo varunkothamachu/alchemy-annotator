4f47e0bbae16e6d5b23eaf6b9f6d12e3ee16a24f
tommaso.teofili <tommaso.teofili@38d1c562-be07-11de-b431-8336cfaced44>
2009-11-25 17:02:35 +0000

    *added json parser for ranked entity extraction *modified site descriptor

--- trunk/src/main/java/org/apache/uima/alchemy/digester/domain/AnnotatedResults.java
+++ trunk/src/main/java/org/apache/uima/alchemy/digester/domain/AnnotatedResults.java
@@ -20,19 +20,9 @@ package org.apache.uima.alchemy.digester.domain;
 
 public class AnnotatedResults extends Results {
 
-  private String url;
-
   private String text;
 
   private String annotatedText;
-
-  public String getUrl() {
-    return url;
-  }
-
-  public void setUrl(String url) {
-    this.url = url;
-  }
 
   public String getText() {
     return text;
---
+++ trunk/src/test/resources/jsonTextRankedEntityResult.js
@@ -0,0 +1,37 @@
+{
+    "status": "OK",
+    "url": "",
+    "language": "english",
+    "entities": [
+        {
+            "type": "Country",
+            "relevance": "0.33",
+            "count": "1",
+            "text": "Afghanistan",
+            "disambiguated": {
+                "name": "Afghanistan",
+                "geo": "34.5166666667 69.1333333333",
+                "dbpedia": "http://dbpedia.org/resource/Afghanistan",
+                "freebase": "http://rdf.freebase.com/ns/guid.9202a8c04000641f800000000000418c",
+                "ciaFactbook": "http://www4.wiwiss.fu-berlin.de/factbook/resource/Afghanistan",
+                "umbel": "http://umbel.org/umbel/ne/wikipedia/Afghanistan",
+                "opencyc": "http://sw.opencyc.org/concept/Mx4rvViIFpwpEbGdrcN5Y29ycA",
+                "yago": "http://mpii.de/yago/resource/Afghanistan"
+            }
+        },
+        {
+            "type": "Country",
+            "relevance": "0.33",
+            "count": "1",
+            "text": "US",
+            "disambiguated": {
+                "name": "United States",
+                "geo": "38.8833333333 -77.0166666667",
+                "dbpedia": "http://dbpedia.org/resource/United_States",
+                "freebase": "http://rdf.freebase.com/ns/guid.9202a8c04000641f8000000000959f60",
+                "ciaFactbook": "http://www4.wiwiss.fu-berlin.de/factbook/resource/United_States",
+                "yago": "http://mpii.de/yago/resource/United_States"
+            }
+        }
+    ]
+}
